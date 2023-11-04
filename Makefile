dccdc5debbeac8431f48d68b6643105c88ba4140
Clément <clement2104.boillot@gmail.com>
2023-11-04 18:32:14 +0100

    Init project

---
+++ Makefile
@@ -0,0 +1,64 @@
+# ↓ Basic variables
+CC := gcc -std=gnu11
+CFLAGS := -W -Wall -Wextra -Wunused -Wpedantic
+CFLAGS += -Wundef -Wshadow -Wcast-align
+CFLAGS += -Wstrict-prototypes -Wmissing-prototypes
+CFLAGS += -Waggregate-return -Wcast-qual
+CFLAGS += -Wunreachable-code
+CFLAGS += -U_FORTIFY_SOURCE
+CFLAGS += -iquote ./include
+
+# ↓ CSFML
+CSFML_FLAGS := -lm
+CSFML_FLAGS += -lcsfml-window
+CSFML_FLAGS += -lcsfml-system
+CSFML_FLAGS += -lcsfml-graphics
+CSFML_FLAGS += -lcsfml-audio
+CFLAGS += $(CSFML_FLAGS)
+
+# ↓ Binaries
+NAME := ramBouncer
+
+# Source files
+VPATH := src
+SRC :=
+
+# ↓ Objects
+BUILD_DIR := .build
+OBJ := $(SRC:%.c=$(BUILD_DIR)/%.o)
+
+# ↓ Dependencies for headers
+DEPS_FLAGS := -MMD -MP
+DEPS := $(OBJ:.o=.d)
+
+DIE := exit 1
+
+all: $(NAME)
+
+.PHONY: all
+
+# ↓ Compiling
+$(BUILD_DIR)/%.o: %.c
+	@ mkdir -p $(dir $@)
+	$(CC) -o $@ -c $< $(CFLAGS) $(DEPS_FLAGS) || $(DIE)
+
+$(NAME): $(OBJ)
+	$(CC) -o $@ $^ $(CFLAGS) || $(DIE)
+
+.PHONY: $(NAME)
+
+# ↓ Cleaning
+clean:
+	@ $(RM) $(OBJ)
+
+fclean: clean
+	@ $(RM) -r $(BUILD_DIR)
+	@ $(RM) $(NAME)
+
+.PHONY: clean fclean
+
+re: fclean all
+
+.PHONY: re
+
+-include $(DEPS)
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "ramBouncer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cc = pkgs.gcc12;
+
+        deps = with pkgs; [
+          glibc
+          gnumake
+          csfml
+        ] ++ [ cc ];
+
+        mkCApp = name: pkgs.stdenv.mkDerivation rec {
+          inherit name;
+          src = ./.;
+
+          makeFlags = [ "CC=${cc}/bin/gcc" ];
+          buildInputs = deps;
+
+          hardeningDisable = [ "format" "fortify" ];
+          enableParallelBuilding = true;
+
+          buildPhase = ''
+            make ${name} NO_COV=1
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${name} $out/bin
+          '';
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcovr
+            ltrace
+            valgrind
+            python311Packages.compiledb
+            man-pages
+            man-pages-posix
+          ] ++ deps;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          default = mkCApp "ramBouncer";
+        };
+      });
+}
